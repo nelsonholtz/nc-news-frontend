df4a781f21b5d3c0eea4bde5c2fa2d9edbd49b6c
nelsonholtz <nelson_holtz@hotmail.co.uk>
2025-09-29 18:16:36 +0100

    adding further particles

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { Routes, Route } from "react-router-dom";
+import ParticlesBg from "particles-bg";
 
 import Header from "./components/Header";
 import ArticlesPage from "./pages/ArticlesPage";
@@ -14,48 +15,70 @@ function App() {
 
   useEffect(() => {
     const storedUser = localStorage.getItem("loggedInUser");
-    if (storedUser) {
-      setLoggedInUser(JSON.parse(storedUser));
-    }
+    if (storedUser) setLoggedInUser(JSON.parse(storedUser));
   }, []);
 
   return (
-    <div>
-      <header>
-        <Header loggedInUser={loggedInUser} setLoggedInUser={setLoggedInUser} />
-      </header>
+    <>
+      {/* Full-page particles */}
+      <div
+        id="particles-background"
+        style={{
+          position: "fixed", // keeps it fixed on the screen
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%", // full viewport
+          zIndex: 0, // behind content
+        }}
+      >
+        <ParticlesBg
+          type="cobweb"
+          bg={false} // false because the wrapper handles background
+          color="#99ccff"
+          num={120}
+        />
+      </div>
 
-      <main>
-        <Routes>
-          <Route path="/home" element={<HomePage />} />
-          <Route
-            path="/login"
-            element={
-              <LoginRegister
-                loggedInUser={loggedInUser}
-                setLoggedInUser={setLoggedInUser}
-              />
-            }
+      {/* Scrollable content above particles */}
+      <div className="scrollable-content">
+        <header>
+          <Header
+            loggedInUser={loggedInUser}
+            setLoggedInUser={setLoggedInUser}
           />
-          <Route
-            path="/articles"
-            element={<ArticlesPage loggedInUser={loggedInUser} />}
-          />
+        </header>
 
-          <Route
-            path="/post"
-            element={<PostArticlePage loggedInUser={loggedInUser} />}
-          />
-
-          <Route
-            path="/articles/:articleID"
-            element={<SingleArticlePage loggedInUser={loggedInUser} />}
-          />
-
-          <Route path="/topics/:topicSlug" element={<TopicArticlePage />} />
-        </Routes>
-      </main>
-    </div>
+        <main>
+          <Routes>
+            <Route path="/home" element={<HomePage />} />
+            <Route
+              path="/login"
+              element={
+                <LoginRegister
+                  loggedInUser={loggedInUser}
+                  setLoggedInUser={setLoggedInUser}
+                />
+              }
+            />
+            <Route
+              path="/articles"
+              element={<ArticlesPage loggedInUser={loggedInUser} />}
+            />
+            <Route
+              path="/post"
+              element={<PostArticlePage loggedInUser={loggedInUser} />}
+            />
+            <Route
+              path="/articles/:articleID"
+              element={<SingleArticlePage loggedInUser={loggedInUser} />}
+            />
+            <Route path="/topics/:topicSlug" element={<TopicArticlePage />} />
+          </Routes>
+        </main>
+      </div>
+    </>
   );
 }
+
 export default App;
